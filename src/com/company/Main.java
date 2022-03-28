502bab9e18a96b8abdfc1a06c50e997093a66586
Christian Mui <jinhomui27@gmail.com>
2022-03-27 20:20:13 -0700

    Completed challenge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,31 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        isPalindrome(-121);
+        isPalindrome(1234567899);
     }
 
     public static boolean isPalindrome(int number) {
 
-        if (Math.abs(reverse(number)) == Math.abs(number)) {
-            return true;
-        }
-
-        if (number < 0) {
-            number = Math.abs(number);
+        if (reverse(Math.abs(number)) == Math.abs(number)) {
+             System.out.println(reverse(Math.abs(number)));
+            System.out.println("This is a Palindrome");
+             return true;
         }
 
         System.out.println(reverse(number));
-
-        int Digitcount;
-        int count = 0;
-        while (number > 0) {
-            number = number / 10;
-            number = number % 10;
-            count++;
-            Digitcount=0;
-            System.out.println(number);
-
-        }
 
         return false;
     }
@@ -35,24 +22,22 @@ public class Main {
     public static int reverse(int number) {
         int reverse = 0;
         int lastDigit;
-        int count = 0;
 
         while (number > 0) {
+
+            reverse = reverse * 10;
 
             lastDigit = number % 10;
 
             reverse = reverse + lastDigit;
-            System.out.println("First reverse= " + reverse);
-            System.out.println(lastDigit + "*******");
+//            System.out.println("First reverse= " + reverse);
+//            System.out.println(lastDigit + "*******");
 
             number = number / 10;
-            System.out.println("Number= " + number);
+//            System.out.println("Number= " + number);
 
-            reverse = reverse * 10;
-            System.out.println("Reverse= " + reverse);
+//            System.out.println("Reverse= " + reverse);
 
-            count++;
-            System.out.println(count);
         }
         return reverse;
     }
